9b4ffe274d89e865a47fea82e16b44ae6f41d7ac
blestprune <heitor.franca@outlook.com>
2021-05-27 21:42:29 -0300

    Removendo imports não utilizados

--- GeradorCartoesAPI/Controllers/CartoesController.cs
+++ GeradorCartoesAPI/Controllers/CartoesController.cs
@@ -1,13 +1,9 @@
 ﻿using GeradorCartoesAPI.Context;
 using GeradorCartoesAPI.Models;
 using GeradorCartoesAPI.Utils;
-using GeradorCartoesAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeradorCartoesAPI.Controllers
 {
